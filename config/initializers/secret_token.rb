16081f424b3927e5b98c65994eb14a71f2dca26b
Yoshioka Tsuneo <yoshts@fsapple-yoshts.local>
2012-04-30 15:00:06 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checklunch::Application.config.secret_token = 'a40f463faa91b3f95934cc649059009d45609486f5506c855754fa42cfd154fbffba48dc2a2f2cbbd64e86783222995a766b99ff277681272bf8b62c9950ac38'
